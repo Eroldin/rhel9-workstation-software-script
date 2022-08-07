088eb75ef7352358b7a85d3d2dec594089add04d
Eroldin <54679426+Eroldin@users.noreply.github.com>
2022-08-07 17:47:45 +0200

    Delete rhel9-extra.sh

--- rhel9-extra.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo flatpak remote-add --if-not-exists fedora oci+https://registry.fedoraproject.org
-sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-sudo dnf install --nogpgcheck https://mirrors.rpmfusion.org/free/el/rpmfusion-free-release-$(rpm -E %rhel).noarch.rpm https://mirrors.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-$(rpm -E %rhel).noarch.rpm
